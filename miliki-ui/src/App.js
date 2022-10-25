8495b018d6b061b4cf02803bb30711ca845ca136
timmoh-king <timothykariuki43@gmail.com>
2022-10-25 13:02:41 +0300

    Sign in page

--- miliki-ui/src/App.js
+++ miliki-ui/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import MilikiLogo from "./components/common/MilikiLogo";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-4xl font-bold mt-3 text-miliki-black">
-        Welcome to <span className="text-miliki-blue">Miliki</span>{" "}
-        <span className="text-miliki-sky-blue">Investment</span>
+      <h1 className="text-2xl font-bold mt-5 text-miliki-black">
+        Welcome to <span className="text-miliki-blue">Miliki</span> <span className="text-miliki-sky-blue">Investment</span>
       </h1>
     </div>
   );
