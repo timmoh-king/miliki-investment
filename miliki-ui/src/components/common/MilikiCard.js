cc6910f5f2355004710bbe564c160814106f3771
timmoh-king <timothykariuki43@gmail.com>
2022-10-31 17:40:57 +0300

    User dashboard

---
+++ miliki-ui/src/components/common/MilikiCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import MilikiButton from './MilikiButton'
+
+const MilikiCard = ({imgSrc, alt }) => {
+  return (
+    <div>
+      <div className='max-w-[950px] px-1 py-2 mt-3 rounded-md ring-1 ring-miliki-sky-blue'>
+        <img className="w-full h-[256px] px-2 rounded-[9px]" src={imgSrc} alt={alt}/>
+        <p className="px-2 py-4 text-justify">is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since the
+            1500s,is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since the
+            1500s,is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since the
+            1500s,
+        </p>
+        <div className='flex items-start px-2 py-2 ml-4'>
+        <ul className='list-disc'>
+            <li className='text-start'>JavaScript</li>
+            <li className='text-start'>React Js</li>
+            <li className='text-start'>Tailwindcss</li>
+        </ul>
+        </div>
+        <div className='flex flex-row gap-2 px-1'>
+            <MilikiButton isButtonForm={false} buttonStyle="rounded full text-miliki-black" buttonText="Amount"/>
+            <MilikiButton isButtonForm={false} buttonStyle="rounded full text-miliki-black" buttonText="Duration"/>
+            <MilikiButton isButtonForm={false} buttonStyle="rounded full text-miliki-black" buttonText="Status"/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MilikiCard
---
+++ miliki-ui/src/components/pages/MilikiDashboard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import MilikiSidenav from "../common/MilikiSidenav";
+import MilikiNavbar from "../common/MilikiNavbar";
+import MilikiCard from "../common/MilikiCard";
+
+
+const MilikiDashboard = () => {
+  return (
+    <div>
+      <MilikiNavbar />
+      <div className="container flex">
+        <div className="py-1 px-2">
+          <MilikiSidenav isSidenavLeft={true} />
+        </div>
+        <div className="max-w-[956px] ml-[260px] px-2 py-1 mt-1 bg-white rounded-md">
+          <MilikiCard imgSrc="c-dustin-91AQt9p4Mo8-unsplash.jpg" alt="investment" />
+          <MilikiCard imgSrc="avi-waxman-f9qZuKoZYoY-unsplash.jpg" alt="investment" />
+        </div>
+        
+        <div className="py-1 px-2">
+          <MilikiSidenav isSidenavLeft={false} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MilikiDashboard;
