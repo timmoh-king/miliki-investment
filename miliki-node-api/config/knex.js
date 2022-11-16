575608b8124b19779ebbcb1779842bf32b9b6751
timmoh-king <timothykariuki43@gmail.com>
2022-11-16 17:59:25 +0300

    final changes

--- miliki-node-api/config/knex.js
+++ miliki-node-api/config/knex.js
@@ -2,11 +2,6 @@ const config = {
     client: 'pg',
     version: '8.8.0',
     connection: {
-        // host: process.env.DATABASE_HOST,
-        // user: process.env.DATABASE_USER,
-        // port: process.env.DATABASE_PORT,
-        // database: process.env.DATABASE,
-        // password: process.env.DATABASE_USER_PASSWORD,
         host: "localhost",
         user: "postgres",
         port: 5432,
